129eb4e8d20b739602a63fffa9b38fd34a128c65
aper77 <bryanmayis95@gmail.com>
2022-02-03 14:19:26 +0400

    update

--- meta/src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Header() {
-    return (
-        <div class="d-flex flex-row header">
-            <div class="p-2 meta-logo"></div>
-        </div>
-    )
-}
